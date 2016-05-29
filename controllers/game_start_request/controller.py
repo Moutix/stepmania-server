6ff3794c6938326fde73e52888a53c7d782287cf
Selim <slm.menouar@gmail.com>
2016-05-29 18:01:23 +0200

    Add controller for game start request

---
+++ controllers/game_start_request/controller.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from smutils import smpacket
+
+from stepmania_controller import StepmaniaController
+
+import models
+
+class StartGameRequestController(StepmaniaController):
+    command = smpacket.SMClientCommand.NSCRSG
+
+    def handle(self):
+        if not self.room:
+            return
+
+        song = models.Song.find_or_create(
+            self.packet["song_title"],
+            self.packet["song_subtitle"],
+            self.packet["song_artist"],
+            self.session)
+
+        self.send(smpacket.SMPacketServerNSCGSR())
+

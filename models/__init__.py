78e00a0e1122712d672a7f43ab7cc89f75a3a3dd
Selim Menouar <selim.menouar@rez-gif.supelec.fr>
2016-06-03 18:58:23 +0200

    Remove old files

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from models.user import User, UserStatus
-from models.room import Room
-from models.song import Song
-from models.song_stat import SongStat
--- models/schema.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf8 -*-
-
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-

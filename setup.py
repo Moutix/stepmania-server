04be17aee65825c8a2770b5be95aba8d2ac9ad3f
Sélim Menouar <slm.menouar@gmail.com>
2016-07-30 09:15:15 +0200

    Moving to 1.0.0

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
 
     classifiers=[
         'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Stable',
         'License :: OSI Approved :: MIT License',
         'Topic :: Games/Entertainment',
         'Topic :: Games/Entertainment :: Arcade',
